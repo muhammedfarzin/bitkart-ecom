7f11394f7ac2f1064f5dbcea0748057943a3c1fd
Muhammed Farzin <muhammedfarzink5@gmail.com>
2024-05-04 19:52:49 +0530

    admin login page created

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 import express from "express";
 import adminRouter from "./routes/admin.js";
 import dotenv from "dotenv";
+import hbs from 'express-handlebars'
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: 'views/layouts',
+    partialsDir: ['views/admin/partials']
+}))
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,5 +2,10 @@ import { Router } from "express";
 
 const router = Router();
 
+// Routes
+router.get('/login', (req, res) => {
+    res.render('admin/login', { title: "Admin Login" });
+})
+
 
 export default router;
